0d82220deefd415457599c1fc6b37068fc8c5af5
AbramFernandoH <abramfernando18@gmail.com>
2021-07-16 20:00:23 +0700

    Successfully making operations on calculator and change the state to show the result

--- src/App.js
+++ src/App.js
@@ -1,31 +1,13 @@
 import { Component } from "react";
-import Screen from "./Screen";
 import Calculator from "./Calculator";
 import "./App.css";
 
 class App extends Component{
-  constructor(props){
-    super(props);
-    this.state = {
-      calcScreen: '0'
-    }
-    this.clearScreen = this.clearScreen.bind(this);
-  }
-
-  clearScreen(){
-    this.setState(curState => {
-      return { calcScreen: '' }
-    });
-  }
-
   render(){
     return(
       <div className="App">
         <h1 className="title">Calculator using Reactjs</h1>
-        <Screen show={this.state.calcScreen} />
-        <Calculator 
-          clearScreen={this.clearScreen}
-        />
+        <Calculator />
       </div>
     )
   }
--- src/Calculator.js
+++ src/Calculator.js
@@ -1,5 +1,6 @@
 import { Component } from "react";
-import CalcButton from './CalcButton';
+import Screen from "./Screen";
+import CalcButton from "./CalcButton";
 import "./Calculator.css";
 
 class Calculator extends Component{
@@ -8,20 +9,105 @@ class Calculator extends Component{
     calcOperator: [ 'divide', 'times', 'plus', 'minus', 'equals' ]
   }
 
+  constructor(props){
+    super(props);
+    this.state = {
+      calcScreen: '0'
+    }
+    this.clearScreen = this.clearScreen.bind(this);
+    this.addOperators = this.addOperators.bind(this);
+  }
+
+  clearScreen(){
+    this.setState(curState => {
+      return { calcScreen: '' }
+    });
+  }
+
+  addOperators(operator){
+    const operators = /(\/|-|\+|\*)/g;
+    if(operators.test(this.state.calcScreen) === false && this.state.calcScreen !== ''){
+      switch (operator) {
+        case 'divide':
+          this.setState(curState => {
+            return { calcScreen: `${curState.calcScreen} / ` }
+          });
+          break;
+  
+        case 'times':
+          this.setState(curState => {
+            return { calcScreen: `${curState.calcScreen} * ` }
+          });
+          break;
+  
+        case 'plus':
+          this.setState(curState => {
+            return { calcScreen: `${curState.calcScreen} + ` }
+          });
+          break;
+  
+        default:
+          this.setState(curState => {
+            return { calcScreen: `${curState.calcScreen} - ` }
+          });
+          break;
+      }
+    } else if(operator === 'equals' && this.state.calcScreen !== '') {
+      const calcScreen = [...this.state.calcScreen];
+      const operand = calcScreen.splice(calcScreen.findIndex(op => op === ' ') + 1, 1);
+      const firstDigitNum2 = calcScreen.findIndex(space => space === ' ') + 2;
+      const num2 = parseFloat((calcScreen.splice(firstDigitNum2, calcScreen.length - firstDigitNum2)).join('').trim());
+      const num1 = parseFloat(calcScreen.join('').trim());
+      switch (operand[0]) {
+        case "/":
+          this.setState(() => {
+            return { calcScreen: num1 / num2 }
+          });
+          break;
+
+        case "*":
+          this.setState(() => {
+            return { calcScreen: num1 * num2 }
+          });
+          break;
+
+        case "+":
+          this.setState(() => {
+            return { calcScreen: num1 + num2 }
+          });
+          break;
+      
+        case "-":
+          this.setState(() => {
+            return { calcScreen: num1 - num2 }
+          });
+          break;
+      }
+    }
+  }
+
+  addNumber(num){
+    this.setState(curState => {
+      return { calcScreen: `${curState.calcScreen}${num}` }
+    });
+  }
+
   render(){
     const operatorClass = 'fas fa-';
     return(
       <div className="Calculator">
+        <Screen show={this.state.calcScreen} />
         <CalcButton 
           calcBtnClass='clear' 
           calcvalue='CLEAR' 
-          btnClick={this.props.clearScreen} 
+          btnClick={this.clearScreen} 
         />
         <div className="operators">
           {this.props.calcOperator.map(operator =>
             <CalcButton 
               calcBtnClass={operator}
               calcvalue={<i className={`${operatorClass}${operator}`}></i>}
+              btnClick={() => this.addOperators(operator)}
             />
           )}
         </div>
@@ -30,6 +116,7 @@ class Calculator extends Component{
             <CalcButton 
               calcBtnClass={calcNum} 
               calcvalue={calcNum} 
+              btnClick={() => this.addNumber(calcNum)}
             />
           )}
         </div>
